e7fe0d1c942d408aa777fd0972792c6e19813b0c
michlee1337 <michlee1337@gmail.com>
2019-02-19 14:57:28 +0530

    navigation links self generate

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,14 +24,12 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = current_project.comments.build(params[:comment])
 
-    respond_to do |format|
+    if @comment.save
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        flash[:success] = "Comment created!"
       else
-        format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
