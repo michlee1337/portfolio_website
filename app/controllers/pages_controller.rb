5211f24fc61c2809946342cd0ec2ceb591eae5f7
michlee1337 <michlee1337@gmail.com>
2019-02-25 16:04:48 +0530

    updated landing page w web dev photo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,32 @@
 class PagesController < ApplicationController
 
+	def index
+		@projects = Project.all
+	end
+
 	def highlights
 		@title = 'Highlights';
-		@subtitle = 'So far'
+		@subtitle = 'So far';
 	end
 
 	def web_dev
-		@title = 'Web Development'
-		@subtitle = 'Functional Design and Development'
+		@title = 'Web Development';
+		@subtitle = 'Functional Design and Development';
 	end
 
 	def datascience
 		@title = 'Datascience';
-		@subtitle = 'Simple and honest insights'
+		@subtitle = 'Simple and honest insights';
 	end
 
 	def analysis
 		@title = 'Analysis';
-		@subtitle = '80% of the solution is knowing the problem'
+		@subtitle = '80% of the solution is knowing the problem';
 	end
 
 	def about
 		@title = 'About Me';
-		@subtitle = 'A Work in Progress'
+		@subtitle = 'A Work in Progress';
 	end
 
 end
