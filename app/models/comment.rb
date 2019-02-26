0b53b563b7ba33e696d18de8f119cf98a13436b4
michlee1337 <michlee1337@gmail.com>
2019-02-27 00:26:27 +0530

    added work in other categories

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
 	belongs_to :project
 
 	# maximum length
-	validates :content, :length => { :maximum => 140}
+	#validates :content, :length => { :maximum => 140}
 end
